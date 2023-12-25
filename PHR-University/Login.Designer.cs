e41d6a71f91d72d7b3c80fd4c0debca80af3fbff
unknown <elkyhalberstam@gmail.com>
2023-12-25 13:00:25 -0500

    worked on student portal - need to add in proper store procedures

--- PHR-University/Login.Designer.cs
+++ PHR-University/Login.Designer.cs
@@ -31,8 +31,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -64,30 +64,32 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Password ";
             // 
-            // textBox1
+            // username
             // 
-            this.textBox1.Location = new System.Drawing.Point(432, 140);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(237, 26);
-            this.textBox1.TabIndex = 3;
+            this.username.Location = new System.Drawing.Point(432, 140);
+            this.username.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(237, 26);
+            this.username.TabIndex = 3;
             // 
-            // textBox2
+            // password
             // 
-            this.textBox2.Location = new System.Drawing.Point(432, 211);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(237, 26);
-            this.textBox2.TabIndex = 4;
+            this.password.Location = new System.Drawing.Point(432, 211);
+            this.password.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(237, 26);
+            this.password.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(432, 339);
+            this.button1.Location = new System.Drawing.Point(446, 311);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(110, 37);
             this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
+            this.button1.Text = "sumbit";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Login
             // 
@@ -95,8 +97,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 562);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -113,8 +115,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.Button button1;
     }
 }
--- PHR-University/StudentPage.cs
+++ PHR-University/StudentPage.cs
@@ -15,6 +15,7 @@ namespace PHR_University
         public StudentPage()
         {
             InitializeComponent();
+            
         }
 
         private void helloToolStripMenuItem_Click(object sender, EventArgs e)
@@ -25,6 +26,101 @@ namespace PHR_University
         private void scheduleToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void StudentPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void academicToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void homeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Panel home = HomePannel;
+            home.Show();
+            regisratinPanel.Hide();
+            academicPanel.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuRegistratiom_Click(object sender, EventArgs e)
+        {
+            
+            regisratinPanel.Show();
+            HomePannel.Hide();
+            academicPanel.Hide();
+        }
+
+        private void menueAcademics_Click(object sender, EventArgs e)
+        {
+            academicPanel.Show();
+            regisratinPanel.Hide();
+            HomePannel.Hide();
+            
+        }
+
+        private void courseButton_Click(object sender, EventArgs e)
+        {
+            //Form courseSchedule = new courseSchedue();
+            //courseSchedule.Show();
+        }
+
+        private void courseButton_Click_1(object sender, EventArgs e)
+        {
+            Form courseSchedule = new courseSchedue();
+            courseSchedule.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //trancript
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //current grades
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            //graduation status
+        }
+
+        private void lookUPclasses_Click(object sender, EventArgs e)
+        {
+            //look up classes
+            Form lookUp = new lookUpclasses();
+            lookUp.Show();
+        }
+
+        private void addDropButton_Click(object sender, EventArgs e)
+        {
+            //add/drop button
+            Form addDrop = new addDropForm();
+            addDrop.Show();
         }
     }
 }
